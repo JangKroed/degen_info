6ef85d56077658761454cb1ba1ee2020be4dab98
JangKroed <didlsdydgh@gmail.com>
2024-04-25 15:31:50 +0900

    edit content script

--- content-script.js
+++ content-script.js
@@ -1,29 +1,45 @@
-var responseToJson = async (res) => {
-  try {
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
+async function responseToJson(url) {
+  const response = await fetch(url, {
+    method: 'GET',
+    // headers: {
+    //   authority: 'www.degen.tips',
+    //   method: 'GET',
+    //   path: '/api/airdrop2/season3/points?address=0xde8f202db8cddc52797a10bd67faf14ab42c65a5',
+    //   scheme: 'https',
+    //   Accept:
+    //     'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    //   'Accept-Encoding': 'gzip, deflate, br, zstd',
+    //   'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    //   'Cache-Control': 'max-age=0',
+    //   Cookie: "wagmi.store={'state':{'connections':{'__type':'Map','value':[]},'chainId':8453,'current':null},'version':2}",
+    //   Priority: 'u=0, i',
+    //   'Sec-Ch-Ua': "'Chromium';v='124', 'Google Chrome';v='124', 'Not-A.Brand';v='99'",
+    //   'Sec-Ch-Ua-Mobile': '?0',
+    //   'Sec-Ch-Ua-Platform': 'Windows',
+    //   'Sec-Fetch-Dest': 'document',
+    //   'Sec-Fetch-Mode': 'navigate',
+    //   'Sec-Fetch-Site': 'none',
+    //   'Sec-Fetch-User': '?1',
+    //   'Upgrade-Insecure-Requests': '1',
+    //   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    // },
+  });
+  console.log(response);
+  return await response.json().catch(console.error);
+}
 
-    const jsonData = res;
-    console.log(JSON.stringify(res));
-    return Array.isArray(jsonData) ? jsonData[0] : jsonData;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-function setUserinfo(fid) {
+async function setUserinfo(fid) {
   const GET_PROFILE_URL = `https://www.supercast.xyz/api/profile?fid=${fid}`;
-  const { username, avatar, connectedAddress: address, displayName } = fetch(GET_PROFILE_URL).then(responseToJson).catch(console.error);
+  const { user } = await responseToJson(GET_PROFILE_URL);
+  const { username, avatar, connectedAddress: address, displayName } = user;
 
   const USE_POINT_URL = `https://www.degen.tips/api/airdrop2/season3/points?address=${address}`;
   const USE_TIP_URL = `https://www.degen.tips/api/airdrop2/tip-allowance?address=${address}`;
 
-  console.error(USE_POINT_URL);
+  const [getPointInfo] = await responseToJson(USE_POINT_URL);
+  const { points: my_point } = getPointInfo;
 
-  const { points: my_point } = fetch(USE_POINT_URL).then(responseToJson).catch(console.error);
-  const { user_rank, tip_allowance, remaining_allowance } = fetch(USE_TIP_URL).then(responseToJson).catch(console.error);
+  const { user_rank, tip_allowance, remaining_allowance } = (await responseToJson(USE_TIP_URL))[0];
 
   const userInfo = {
     username,
@@ -36,6 +52,8 @@ function setUserinfo(fid) {
     remaining_allowance,
   };
 
+  console.error(JSON.stringify(userInfo));
+
   chrome.runtime.sendMessage({ action: 'LOGIN_SUCCESS', fid, address, userInfo });
 }
 
@@ -43,13 +61,15 @@ function getItem(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-var data = getItem('currentSupercastFID');
+(async () => {
+  const data = getItem('currentSupercastFID');
 
-if (data) {
-  setUserinfo(data);
-} else {
-  const item = getItem('REACT_QUERY_OFFLINE_CACHE');
-  if (item?.clientState) {
-    setUserinfo(item.clientState.queries[0].queryKey[1]);
+  if (data) {
+    await setUserinfo(data);
+  } else {
+    const item = getItem('REACT_QUERY_OFFLINE_CACHE');
+    if (item?.clientState) {
+      await setUserinfo(item.clientState.queries[0].queryKey[1]);
+    }
   }
-}
+})();
